cd10dbdbd500b125d128e13ba8a011f546a42a50
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2018-02-01 13:47:40 -0800

    playing with path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,10 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 print('\nCurrent path: ')
 print(os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('../../otherpackage'))
+# sys.path.insert(0, os.path.abspath('../..'))
 print('System path:\n')
 print(sys.path)
 print('\n\n')
